508899cc2d126272b5b63a5af89724b895798a00
snehaljinrale-coder <snehaljinrale@gmail.com>
2025-10-17 09:48:46 +0530

    Implement addition and subtraction in math.py
    Added basic arithmetic operations with error handling.

---
+++ math.py
@@ -0,0 +1,10 @@
+#math.py
+try:
+  a =float(input("enter first number :"))
+  b = float(input("enter second number :"))
+
+  print (f" addition : {a + b }")
+  print(f"subtraction: { a - b}")
+ 
+except valueError :
+   print ("please enter valid number ")
